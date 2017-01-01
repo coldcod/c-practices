cc0636d78b9f7c005f39d3a8ef7bef1bd17a0f4f
satwinderpal <satwindersapra@ymail.com>
2017-01-01 21:11:55 +0530

    Initial Commit

---
+++ c-practices/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-practices
+//
+//  Created by Satwinderpal Singh on 01/01/17.
+//  Copyright © 2017 Satwinderpal Singh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
